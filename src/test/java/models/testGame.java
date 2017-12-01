d0e9078365f65fbefd8c4182bc5b0f6b9f2d683d
Alexandria Wilson <wilsalex@oregonstate.edu>
2017-11-30 19:36:50 -0800

    Fixed files?

--- src/test/java/models/testGame.java
+++ src/test/java/models/testGame.java
@@ -2,11 +2,10 @@ package models;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
-/**
- * Created by michaelhilton on 1/25/16.
- */
 public class testGame {
 
     @Test
@@ -18,48 +17,46 @@ public class testGame {
     @Test
     public void testGameBuildDeck(){
         Game g = new Game();
-        g.buildDeck();
-        assertEquals(52,g.deck.size());
+        assertEquals(52,g.deck.cards.size());
     }
 
     @Test
-    public void testGameInit(){
-        Game g = new Game();
-        g.buildDeck();
-        g.shuffle();
-        assertNotEquals(2,g.deck.get(0).getValue());
+    public void testGameShuffle(){
+        Game g1 = new Game();
+        g1.deck.shuffle();
+        Game g2 = new Game();
+        g2.deck.shuffle();
+        // g1 and g2 could shuffle to the same order, but that chance is approximately 1 in 8*10^67 shuffles
+        assertFalse(Arrays.equals(g1.deck.cards.toArray(),g2.deck.cards.toArray()));
     }
 
     @Test
     public void testGameStart(){
         Game g = new Game();
-        g.buildDeck();
-        g.shuffle();
+        g.deck.shuffle();
         g.dealFour();
-        assertEquals(1,g.cols.get(0).size());
-        assertEquals(1,g.cols.get(1).size());
-        assertEquals(1,g.cols.get(2).size());
-        assertEquals(1,g.cols.get(3).size());
+        assertEquals(1,g.columns.get(0).cards.size());
+        assertEquals(1,g.columns.get(1).cards.size());
+        assertEquals(1,g.columns.get(2).cards.size());
+        assertEquals(1,g.columns.get(3).cards.size());
     }
 
     @Test
     public void testCustomDeal(){
         Game g = new Game();
-        g.buildDeck();
         g.customDeal(0,3,6,9);
-        assertEquals("2Clubs",g.cols.get(0).get(0).toString());
-        assertEquals("3Clubs",g.cols.get(1).get(0).toString());
-        assertEquals("4Clubs",g.cols.get(2).get(0).toString());
-        assertEquals("5Clubs",g.cols.get(3).get(0).toString());
+        assertEquals("2Clubs",g.columns.get(0).cards.get(0).toString());
+        assertEquals("3Clubs",g.columns.get(1).cards.get(0).toString());
+        assertEquals("4Clubs",g.columns.get(2).cards.get(0).toString());
+        assertEquals("5Clubs",g.columns.get(3).cards.get(0).toString());
     }
 
     @Test
     public void testRemoveFunction(){
         Game g = new Game();
-        g.buildDeck();
         g.customDeal(0,3,6,9);
         g.remove(2);
-        assertEquals(0,g.cols.get(2).size());
+        assertEquals(0,g.columns.get(2).cards.size());
     }
 
 
