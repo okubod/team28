fcdc3253b604d3d5df92f6733681e90278c2c550
Alexandria Wilson <wilsalex@oregonstate.edu>
2017-12-01 00:06:34 -0800

    Test suite at 100% coverage.

--- src/test/java/models/testGame.java
+++ src/test/java/models/testGame.java
@@ -1,8 +1,10 @@
 package models;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.EmptyStackException;
 
 import static org.junit.Assert.*;
 
@@ -15,50 +17,92 @@ public class testGame {
     }
 
     @Test
-    public void testGameBuildDeck(){
-        Game g = new Game();
-        assertEquals(52,g.deck.cards.size());
-    }
-
-    @Test
-    public void testGameShuffle(){
-        Game g1 = new Game();
-        g1.deck.shuffle();
-        Game g2 = new Game();
-        g2.deck.shuffle();
-        // g1 and g2 could shuffle to the same order, but that chance is approximately 1 in 8*10^67 shuffles
-        assertFalse(Arrays.equals(g1.deck.cards.toArray(),g2.deck.cards.toArray()));
-    }
-
-    @Test
     public void testGameStart(){
-        Game g = new Game();
-        g.deck.shuffle();
+        Game g = new Game(GameType.Standard);
+        g.resetDeck();
+        g.shuffle();
         g.dealFour();
-        assertEquals(1,g.columns.get(0).cards.size());
-        assertEquals(1,g.columns.get(1).cards.size());
-        assertEquals(1,g.columns.get(2).cards.size());
-        assertEquals(1,g.columns.get(3).cards.size());
+        assertEquals(1,g.cols.get(0).cards.size());
+        assertEquals(1,g.cols.get(1).cards.size());
+        assertEquals(1,g.cols.get(2).cards.size());
+        assertEquals(1,g.cols.get(3).cards.size());
     }
 
     @Test
-    public void testCustomDeal(){
-        Game g = new Game();
-        g.customDeal(0,3,6,9);
-        assertEquals("2Clubs",g.columns.get(0).cards.get(0).toString());
-        assertEquals("3Clubs",g.columns.get(1).cards.get(0).toString());
-        assertEquals("4Clubs",g.columns.get(2).cards.get(0).toString());
-        assertEquals("5Clubs",g.columns.get(3).cards.get(0).toString());
+    public void testGameDealEmpty(){
+        Game g = new Game(GameType.Standard);
+        g.resetDeck();
+        g.shuffle();
+        for(int i = 0; i < 13; i++){
+            g.dealFour();
+        }
+        assertEquals(0, g.deck.theDeck.size());
+        g.dealFour();
+        assertTrue(g.gameOver);
     }
 
     @Test
-    public void testRemoveFunction(){
-        Game g = new Game();
-        g.customDeal(0,3,6,9);
-        g.remove(2);
-        assertEquals(0,g.columns.get(2).cards.size());
+    public void testGameRemove(){
+        Game g = new Game(GameType.Standard);
+        g.resetDeck();
+        g.dealFour();
+        assertEquals("14Spades", g.cols.get(0).readTopCard().toString());
+        boolean removeFailed = false;
+        try {
+            g.remove(0);
+        }
+        catch (Error e){
+            removeFailed = true;
+        }
+        assertTrue(removeFailed);
+        Card c = new Card(3, Suit.Spades);
+        g.cols.get(1).addCardToCol(c);
+        assertEquals("3Spades", g.cols.get(1).readTopCard().toString());
+        boolean removePassed = false;
+        try {
+            g.remove(1);
+            removePassed = true;
+        }
+        catch (Error e){
+            removePassed = false;
+        }
+        assertTrue(removePassed);
     }
 
+    @Test
+    public void testGameMoveSuccess() {
+        Game g = new Game(GameType.Standard);
+        //Test for working case
+        Card c = new Card(14, Suit.Spades);
+        g.cols.get(0).addCardToCol(c);
+        assertEquals("14Spades", g.cols.get(0).readTopCard().toString());
+        g.move(0, 1);
+        assertEquals("14Spades", g.cols.get(1).readTopCard().toString());
+    }
 
+    @Test
+    public void testGameMoveFail(){
+        Game g = new Game(GameType.Standard);
+        //Test for fail case
+        Card c = new Card(3, Suit.Spades);
+        g.cols.get(0).addCardToCol(c);
+        assertEquals("3Spades", g.cols.get(0).readTopCard().toString());
+        g.move(0,1);
+        assertEquals("3Spades", g.cols.get(0).readTopCard().toString());
+    }
+
+    @Test
+    public void testGameCheckEnd(){
+        Game g = new Game(GameType.Standard);
+        g.resetDeck();
+        assertFalse(g.check_end());
+        g.dealFour();
+        assertEquals("14Spades", g.cols.get(0).readTopCard().toString());
+        assertEquals("14Diamonds", g.cols.get(1).readTopCard().toString());
+        assertEquals("14Hearts", g.cols.get(2).readTopCard().toString());
+        assertEquals("14Clubs", g.cols.get(3).readTopCard().toString());
+        g.score = 48;
+        assertTrue(g.check_end());
+    }
 
 }
